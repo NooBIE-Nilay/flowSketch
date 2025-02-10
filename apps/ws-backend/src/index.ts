922d646b3a0c324d5a275201d242f36222dd3231
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-02-11 03:10:24 +0530

    Refactored WS-Backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -2,15 +2,21 @@ import { WebSocketServer } from "ws";
 import jwt, { decode, JwtPayload } from "jsonwebtoken";
 import { JWT_SECRET, WS_PORT } from "@repo/backend-common/config";
 const PORT = WS_PORT;
+function validateUser(token: string) {
+  const decoded = jwt.verify(token, JWT_SECRET);
+  if (!decoded || typeof decoded == "string" || !decoded.userId) return null;
+  return decoded.userId;
+}
 const wss = new WebSocketServer({ port: PORT });
 wss.on("connection", function connection(ws, req) {
   const token = req.headers["authorization"] ?? "";
-  const decoded = jwt.verify(token, JWT_SECRET);
-  if (!decoded || !(decoded as JwtPayload).userId) {
+  const userId = validateUser(token);
+  if (!userId) {
     ws.send("Invalid JWT");
     ws.close();
     return;
   }
+
   ws.on("message", function message(data) {
     ws.send("ping!");
   });
