d5a3c261ea133f5c72276c33432ff7aa4b94468c
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-02-11 13:23:54 +0530

    Added WS Logic for chat-room

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,15 +1,27 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET, WS_PORT } from "@repo/backend-common/config";
 const PORT = WS_PORT;
+import { prisma } from "@repo/db/client";
+
+interface User {
+  ws: WebSocket;
+  rooms: string[];
+  userId: string;
+}
+const users: User[] = [];
 function validateUser(token: string) {
-  const decoded = jwt.verify(token, JWT_SECRET);
-  if (!decoded || typeof decoded == "string" || !decoded.userId) return null;
-  return decoded.userId;
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if (!decoded || typeof decoded == "string" || !decoded.userId) return null;
+    return decoded.userId;
+  } catch (e) {
+    return null;
+  }
 }
 const wss = new WebSocketServer({ port: PORT });
 console.log("Server Running in PORT: ", PORT);
-wss.on("connection", function connection(ws, req) {
+wss.on("connection", (ws, req) => {
   const token = req.headers["authorization"] ?? "";
   const userId = validateUser(token);
   if (!userId) {
@@ -17,7 +29,63 @@ wss.on("connection", function connection(ws, req) {
     ws.close();
     return;
   }
-  ws.on("message", function message(data) {
-    ws.send("ping!");
+  users.push({
+    userId,
+    rooms: [],
+    ws,
+  });
+  ws.on("message", async (data) => {
+    try {
+      const parsedData = JSON.parse(data.toString());
+      if (parsedData.type === "join_room") {
+        const user = users.find((x) => x.ws === ws);
+        // TODO: Check in DB if room exists & then push
+        // TODO: Later add access control so we can introduce private rooms
+        user?.rooms.push(parsedData.roomId);
+        console.log("Someone Joined Room ", parsedData.roomId);
+      }
+      if (parsedData.type === "leave_room") {
+        const user = users.find((x) => x.ws === ws);
+        if (user)
+          user.rooms = user?.rooms.filter((x) => x === parsedData.roomId);
+        console.log("Someone Left Room ", parsedData.roomId);
+      }
+      if (parsedData.type === "chat") {
+        //TODO: Fix: a person not present in room1 can send meesages to room1
+        const { roomId, message } = parsedData;
+        const sender = users.find((x) => x.ws === ws);
+        users.forEach((user) => {
+          if (user.rooms.includes(roomId)) {
+            user.ws.send(
+              JSON.stringify({
+                type: "chat",
+                message,
+                roomId,
+                senderId: sender?.userId,
+              })
+            );
+          }
+        });
+        try {
+          await prisma.chat.create({
+            data: {
+              message,
+              userId,
+              roomId,
+            },
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    } catch (e) {
+      ws.send(JSON.stringify({ message: "Error Pasring Data" }));
+    }
+  });
+  ws.on("close", () => {
+    const index = users.findIndex((user) => user.ws === ws);
+    if (index !== -1) {
+      users.splice(index, 1);
+    }
   });
 });
