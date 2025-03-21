ff2a6b713541b83e96d894f8324a7cdbe0df24b1
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-03-22 03:19:04 +0530

    Added Pencil Tool & Started Selection Tool Fix

--- apps/client/components/Canvas.tsx
+++ apps/client/components/Canvas.tsx
@@ -6,6 +6,9 @@ import { Tools } from "@/lib/config";
 import { useHistory } from "@/hooks/usehistory";
 import { useTheme } from "next-themes";
 import { ModeToggle } from "./modeToggle";
+import { RoughCanvas } from "roughjs/bin/canvas";
+import getStroke from "perfect-freehand";
+import { Diameter } from "lucide-react";
 const generator = rough.generator();
 
 // TODO: Refactor Code
@@ -16,13 +19,17 @@ type element_type = {
   y1: number;
   x2: number;
   y2: number;
-  roughElement: Drawable;
+  roughElement?: Drawable;
+  points?: point[];
   color: string;
 };
+type point = number[];
 interface selected_element_type extends element_type {
-  offsetX: number;
-  offsetY: number;
+  offsetX?: number;
+  offsetY?: number;
   position: string | null;
+  offsetXArray?: number[];
+  offsetYArray?: number[];
 }
 
 export default function Canvas({
@@ -47,14 +54,54 @@ export default function Canvas({
     if (canvasRef.current) {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
-      ctx?.clearRect(0, 0, canvas.width, canvas.height);
+      if (!ctx) return;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
       const roughCanvas = rough.canvas(canvas);
-      elements.forEach(({ roughElement }) => {
-        roughElement.options.stroke = strokeColor;
-        roughCanvas.draw(roughElement);
-      });
+      elements.forEach((element) => renderElement(roughCanvas, ctx, element));
     }
   }, [elements, strokeColor]);
+  const average = (a: number, b: number) => (a + b) / 2;
+
+  function getSvgPathFromStroke(points: point[], closed = true) {
+    const len = points.length;
+    if (len < 4) {
+      return ``;
+    }
+    let a = points[0];
+    let b = points[1];
+    const c = points[2];
+    // @ts-ignore
+    let result = `M${a[0].toFixed(2)},${a[1].toFixed(2)} Q${b[0].toFixed(2)},${b[1].toFixed(2)} ${average(b[0], c[0]).toFixed(2)},${average(b[1], c[1]).toFixed(2)} T`;
+    for (let i = 2, max = len - 1; i < max; i++) {
+      a = points[i];
+      b = points[i + 1];
+      // @ts-ignore
+      result += `${average(a[0], b[0]).toFixed(2)},${average(a[1], b[1]).toFixed(2)} `;
+    }
+    if (closed) {
+      result += "Z";
+    }
+    return result;
+  }
+  const renderElement = (
+    roughCanvas: RoughCanvas,
+    ctx: CanvasRenderingContext2D,
+    element: element_type
+  ) => {
+    //element.roughElement exists for all primitive shapes except for  pencil tool
+    if (element.roughElement) {
+      element.roughElement.options.stroke = strokeColor;
+      roughCanvas.draw(element.roughElement);
+    } else {
+      //code for pencil tool
+      if (!element.points) return;
+      const stroke = getSvgPathFromStroke(
+        getStroke(element.points, { size: 8 })
+      );
+      ctx.fillStyle = strokeColor;
+      ctx.fill(new Path2D(stroke));
+    }
+  };
   const ClearCanvas = () => {
     setElements([]);
   };
@@ -79,11 +126,13 @@ export default function Canvas({
         roughElement = generator.circle(
           x1,
           y1,
-          2 * distance({ x: x1, y: y1 }, { x: x2, y: y2 })
+          2 * distance([x1, y1], [x2, y2])
         );
         break;
       case Tools.PENCIL:
-      //TODO: Add Pencil Logic
+        //TODO: Add Pencil Logic
+        roughElement = undefined;
+        return { id, tool, color, points: [[x1, y1]], x1, y1, x2, y2 };
       default:
         throw new Error(`Tool Not Recognized ${tool}`);
     }
@@ -99,8 +148,12 @@ export default function Canvas({
       .find((element) => element.position !== null);
   };
 
-  const distance = (a: { x: number; y: number }, b: { x: number; y: number }) =>
-    Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+  const distance = (a: point, b: point) => {
+    if (a[0] && b[0] && a[1] && b[1]) {
+      return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+    }
+    return 0; // Return 0 if inputs are invalid
+  };
 
   const nearPoint = (
     x: number,
@@ -111,39 +164,50 @@ export default function Canvas({
   ) => {
     return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? name : null;
   };
-
+  const onLine = (v1: point, v2: point, p: point, maxOffset = 1) => {
+    const offset = distance(v1, v2) - (distance(v1, p) + distance(v2, p));
+    return Math.abs(offset) < maxOffset ? "inside" : null;
+  };
   const getPositionWithinElement = (
     x: number,
     y: number,
     element: element_type
   ) => {
     const { x1, y1, x2, y2 } = element;
-    if (element.roughElement.shape === "line") {
-      const a = { x: x1, y: y1 };
-      const b = { x: x2, y: y2 };
-      const c = { x, y };
-      const offset = distance(a, b) - (distance(a, c) + distance(b, c));
-      const inside = Math.abs(offset) < 1 ? "inside" : null;
-      const start = nearPoint(x, y, x1, y1, "start");
-      const end = nearPoint(x, y, x2, y2, "end");
-      return start || end || inside;
-    } else if (element.roughElement.shape === "rectangle") {
-      const topLeft = nearPoint(x, y, x1, y1, "tl");
-      const topRight = nearPoint(x, y, x2, y1, "tr");
-      const bottomLeft = nearPoint(x, y, x1, y2, "bl");
-      const bottomRight = nearPoint(x, y, x2, y2, "br");
-      const inside = x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
-      return topLeft || topRight || bottomLeft || bottomRight || inside;
-    } else if (element.roughElement.shape === "circle") {
-      const radius = distance({ x: x1, y: y1 }, { x: x2, y: y2 });
-      const a = { x: x1, y: y1 };
-      const c = { x, y };
-      const inside = distance(a, c) <= radius ? "inside" : null;
-      const nearCircumferance =
-        Math.abs(distance(a, c) - radius) <= 5 ? "near" : null;
-      return nearCircumferance || inside;
+    switch (element.tool) {
+      case Tools.LINE:
+        const insideLine = onLine([x1, y1], [x2, y2], [x, y]);
+        const start = nearPoint(x, y, x1, y1, "start");
+        const end = nearPoint(x, y, x2, y2, "end");
+        return start || end || insideLine;
+        break;
+      case Tools.RECTANGLE:
+        const topLeft = nearPoint(x, y, x1, y1, "tl");
+        const topRight = nearPoint(x, y, x2, y1, "tr");
+        const bottomLeft = nearPoint(x, y, x1, y2, "bl");
+        const bottomRight = nearPoint(x, y, x2, y2, "br");
+        const insideRect =
+          x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
+        return topLeft || topRight || bottomLeft || bottomRight || insideRect;
+      case Tools.CIRCLE:
+        const radius = distance([x1, y1], [x2, y2]);
+        const a = [x1, y1];
+        const b = [x, y];
+        const insideCircle = distance(a, b) <= radius ? "inside" : null;
+        const nearCircumferance =
+          Math.abs(distance(a, b) - radius) <= 5 ? "near" : null;
+        return nearCircumferance || insideCircle;
+      case Tools.PENCIL:
+        const betweenAnyPoint = element.points?.some((point, index) => {
+          if (!element.points) return false;
+          const nextPoint = element.points[index + 1];
+          if (!nextPoint) return false;
+          return onLine(point, nextPoint, [x, y], 5) != null;
+        });
+        return betweenAnyPoint ? "inside" : null;
+      default:
+        return null;
     }
-    return null;
   };
   const standardiseElementCoordinates = (element: element_type) => {
     const { x1, y1, x2, y2, tool } = element;
@@ -165,11 +229,26 @@ export default function Canvas({
     y1: number,
     x2: number,
     y2: number,
-    type: string
+    tool: string
   ) => {
-    const updatedElement = createElement(index, x1, y1, x2, y2, type);
     const updatedElements = [...elements];
-    updatedElements[index] = updatedElement;
+    switch (tool) {
+      case Tools.RECTANGLE:
+      case Tools.CIRCLE:
+      case Tools.LINE:
+        updatedElements[index] = createElement(index, x1, y1, x2, y2, tool);
+        break;
+      case Tools.PENCIL:
+        //TODO: Very Ugly Code, Need To Fix
+        const latestPoints = updatedElements[index]?.points;
+        if (!latestPoints) return;
+        const latestPoint = latestPoints[latestPoints?.length - 1];
+        if (latestPoint && distance(latestPoint, [x2, y2]) <= 5) break;
+        updatedElements[index]?.points?.push([x2, y2]);
+        break;
+      default:
+        throw new Error(`Type Not Recognised: ${tool}`);
+    }
     setElements(updatedElements, true);
   };
   const cursorForPosition = (position: string | null) => {
@@ -220,10 +299,26 @@ export default function Canvas({
         if (!selectedElement) return;
         if (selectedElement.position === "inside") setAction("moving");
         else setAction("resizing");
-        const offsetX = clientX - selectedElement.x1;
-        const offsetY = clientY - selectedElement.y1;
-
-        setSelectedElement({ ...selectedElement, offsetX, offsetY });
+        if (selectedElement.tool === Tools.PENCIL) {
+          if (!selectedElement.points) return;
+          const offsetXArray = selectedElement.points.map((point) => {
+            if (!point) return clientX - point[0];
+            else return 0;
+          });
+          const offsetYArray = selectedElement.points?.map((point) => {
+            if (!point) return clientY - point[1];
+            else return 0;
+          });
+          setSelectedElement({
+            ...selectedElement,
+            offsetXArray,
+            offsetYArray,
+          });
+        } else {
+          const offsetX = clientX - selectedElement.x1;
+          const offsetY = clientY - selectedElement.y1;
+          setSelectedElement({ ...selectedElement, offsetX, offsetY });
+        }
         setElements((prevState) => prevState);
       } else {
         setAction("drawing");
@@ -263,19 +358,43 @@ export default function Canvas({
         updateEelement(index, x1, y1, clientX, clientY, selectedTool);
       }
     } else if (action === "moving" && selectedElement) {
-      const { id, x1, y1, x2, y2, offsetX, offsetY, tool } = selectedElement;
-      const width = x2 - x1;
-      const height = y2 - y1;
-      const updatedX = offsetX ? clientX - offsetX : clientX;
-      const updatedY = offsetY ? clientY - offsetY : clientY;
-      updateEelement(
-        id,
-        updatedX,
-        updatedY,
-        updatedX + width,
-        updatedY + height,
-        tool
-      );
+      const { id, x1, y1, x2, y2, tool } = selectedElement;
+      if (selectedElement.tool === Tools.PENCIL) {
+        if (!selectedElement.points) return;
+        const updatedPoints = selectedElement.points.map((point, index) => {
+          if (
+            !selectedElement.offsetXArray ||
+            !selectedElement.offsetYArray ||
+            !selectedElement.offsetXArray[index] ||
+            !selectedElement.offsetYArray[index]
+          )
+            return point;
+          return [
+            selectedElement.offsetXArray[index],
+            selectedElement.offsetYArray[index],
+          ];
+        });
+        const currentElements = [...elements];
+        console.log("Old:", currentElements, id);
+        if (!currentElements || !currentElements[id]) return;
+        currentElements[id].points = updatedPoints;
+        console.log("New:", currentElements);
+        setElements(currentElements);
+      } else {
+        const { offsetX, offsetY } = selectedElement;
+        const width = x2 - x1;
+        const height = y2 - y1;
+        const updatedX = offsetX ? clientX - offsetX : clientX;
+        const updatedY = offsetY ? clientY - offsetY : clientY;
+        updateEelement(
+          id,
+          updatedX,
+          updatedY,
+          updatedX + width,
+          updatedY + height,
+          tool
+        );
+      }
     } else if (action === "resizing" && selectedElement) {
       const { id, tool, position, ...coordinates } = selectedElement;
       const { x1, x2, y1, y2 } = resizedCoordinates(
@@ -289,7 +408,11 @@ export default function Canvas({
   };
   const handleMouseUp = (e: React.MouseEvent<HTMLCanvasElement>) => {
     if (e.button == 0) {
-      if ((action === "drawing" || action === "resizing") && selectedElement) {
+      if (
+        (action === "drawing" || action === "resizing") &&
+        selectedElement &&
+        selectedTool !== Tools.PENCIL
+      ) {
         const index = selectedElement.id;
         if (!elements[index]) return;
         const { id, tool } = elements[index];
